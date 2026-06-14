e1b533b794cd560aa875adb3f01771ec93ce6aee
Ateo <tereshkov896@gmail.com>
2026-06-15 02:36:55 +0700

    Add files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,10 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+    implementation(libs.androidx.glance.appwidget)
+    implementation(libs.androidx.glance.material3)
+    // Для периодических фоновых обновлений (WorkManager)
+    implementation("androidx.work:work-runtime-ktx:2.9.1")
+    // Для работы с корутинами (обычно уже есть)
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
